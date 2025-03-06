29edf0529cbc1647a6d1ea5ff525a1a661c783fc
radhika-varfa <rvarfa93@gmail.com>
2025-03-06 16:39:29 +0530

    changes in server

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  // "http://localhost:5173",
-  "https://melodious-biscochitos-feb6ac.netlify.app/", // replace with your deployed site URL
+  "http://localhost:5173", // For local development
+  "https://melodious-biscochitos-feb6ac.netlify.app", // For production
 ];
 
 app.use(
